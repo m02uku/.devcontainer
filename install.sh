17fe29e76250f630431f3c58be44d1e7309e91ab
m02uku <157340743+m02uku@users.noreply.github.com>
2024-05-28 18:12:53 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-cp -r m02uku-devcontainer/.devcontainer . && cp -r m02uku-devcontainer/.vscode .
+cp -r m02uku-devcontainer/.devcontainer . && cp -r m02uku-devcontainer/.vscode . && echo "m02uku-devcontainer installed!"
