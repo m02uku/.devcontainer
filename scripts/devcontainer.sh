d6c4e145a1f6f6fffeaeb6e300835a9f00b5a6d1
m02uku <m02ukuzrzr@gmail.com>
2024-06-24 04:08:27 +0000

    chore: Update devcontainer and submodule paths in scripts

--- scripts/devcontainer.sh
+++ scripts/devcontainer.sh
@@ -1,5 +1,5 @@
-cp -rf devcontainer/.devcontainer .
-cp -rf devcontainer/.vscode .
-cp -r devcontainer/source/.gitignore .
+cp -rf submodules/devcontainer/.devcontainer .
+cp -rf submodules/devcontainer/.vscode .
+cp -r submodules/devcontainer/source/.gitignore .
 
 echo "**devcontainer installed!**"
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,3 @@
-cd "$(dirname "$0")"
-
-bash devcontainer/scripts/devcontainer.sh
-bash devcontainer/scripts/submodule.sh
-bash devcontainer/scripts/python_env.sh
+bash submodules/devcontainer/scripts/devcontainer.sh
+bash submodules/devcontainer/scripts/submodule.sh
+bash submodules/devcontainer/scripts/python_env.sh
