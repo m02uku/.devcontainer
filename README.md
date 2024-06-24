7521239958b8bb9b5bc3ddf64c1b89d7a0306779
m02uku <m02ukuzrzr@gmail.com>
2024-06-24 14:15:07 +0000

    chore: Refactor devcontainer initialization scripts

--- README.md
+++ README.md
@@ -7,16 +7,21 @@
 ```bash
 git submodule add https://github.com/m02uku/devcontainer.git submodules/devcontainer
 sed -i 's/\r$//' submodules/devcontainer/scripts/*.sh
-bash submodules/devcontainer/scripts/devcontainer.sh
+bash submodules/devcontainer/scripts/init.sh --dev
 ```
 
-Devcontainer に入ってから以下を実行
+Devcontainer に入ってから、必要に応じて以下を実行
 
 ```bash
-bash submodules/devcontainer/scripts/init.sh
+# src ディレクトリを作成
+bash submodules/devcontainer/scripts/init.sh --src
+# Python 仮想環境を構築
+bash submodules/devcontainer/scripts/init.sh --py
+# サブモジュールの依存関係を初期化
+bash submodules/devcontainer/scripts/init.sh --mod
 ```
 
-サブモジュールを更新する場合
+サブモジュールを、リモートリポジトリに設定されているブランチの最新コミットに更新する場合
 
 ```bash
 git submodule update --remote (<submodule path>)
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,35 @@
-# bash submodules/devcontainer/scripts/devcontainer.sh
-bash submodules/devcontainer/scripts/submodule.sh
-bash submodules/devcontainer/scripts/python_env.sh
-bash submodules/devcontainer/scripts/src.sh
+#!/bin/bash
+
+# Check if no arguments are provided
+if [ $# -eq 0 ]; then
+    echo "Warning: No options provided."
+    exit 1
+fi
+
+# Parse command line options and execute corresponding scripts
+for arg in "$@"
+do
+  case $arg in
+    --dev)
+      ./submodules/devcontainer/scripts/devcontainer.sh
+      ;;
+    --py)
+      ./submodules/devcontainer/scripts/python_env.sh
+      ;;
+    --src)
+      ./submodules/devcontainer/scripts/src.sh
+      ;;
+    --mod)
+      ./submodules/devcontainer/scripts/submodules.sh
+      ;;
+    --all)
+      ./submodules/devcontainer/scripts/devcontainer.sh
+      ./submodules/devcontainer/scripts/python_env.sh
+      ./submodules/devcontainer/scripts/src.sh
+      ./submodules/devcontainer/scripts/submodules.sh
+      ;;
+    *)
+      echo "Unknown option: $arg"
+      ;;
+  esac
+done
