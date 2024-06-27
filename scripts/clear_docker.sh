c1fb8b46755189a4470c60e91295b3f5bcacea84
m02uku <m02ukuzrzr@gmail.com>
2024-06-27 23:35:56 +0000

    chore: Update devcontainer Dockerfile to include Codon installation and Python environment setup

--- scripts/clear_docker.sh
+++ scripts/clear_docker.sh
@@ -1,6 +1,9 @@
-docker system prune
-docker container prune
-docker image prune
-docker volume prune
-docker network prune
-docker rmi $(docker images -q)
+yes | docker stop $(docker ps -q)
+yes | docker system prune
+yes | docker container prune
+yes | docker image prune
+yes | docker volume prune
+yes | docker network prune
+yes | docker rmi $(docker images -q)
+docker ps -a
+docker images
--- scripts/python_env.sh
+++ scripts/python_env.sh
@@ -1,7 +1,32 @@
+# 引数がある場合に pyenv を使用して Python のバージョンをインストールする
+if [ ! -z "$1" ]; then
+  pyenv install $1
+  pyenv local $1
+  # CODON_PYTHON 環境変数にパスを設定
+  # バージョン番号を変数に格納
+  version=$1
+
+  # 追加する行を定義
+  export_line="export CODON_PYTHON=\"/root/.pyenv/versions/${version}/lib/libpython3.so\""
+
+  # .bashrcに既に存在するか確認
+  if ! grep -qF -- "$export_line" ~/.bashrc; then
+    # 存在しない場合、行を追加
+    echo "$export_line" >> ~/.bashrc
+    echo ".bashrcにCODON_PYTHONを追加しました。"
+  else
+    echo "CODON_PYTHONは既に.bashrcに存在します。"
+  fi
+fi
+# .bashrcの変更を適用
+source ~/.bashrc
 # Create a virtual environment
 python3 -m venv .venv
 
 source .venv/bin/activate
+
+# Pip
+pip install --upgrade pip
 
 if [ -f requirements.txt ]; then
   pip install -r requirements.txt
@@ -10,3 +35,5 @@ else
 fi
 
 echo "**Python environment created!**"
+echo "Run below to activate the virtual environment:"
+echo "source .venv/bin/activate"
